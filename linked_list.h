7909848e79d06f8244aa8a94f6005bc888d9da2e
rakshithcrm <rakshithcrm@gmail.com>
2023-10-11 19:22:20 +0530

    Global variables changed to const

--- linked_list.h
+++ linked_list.h
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string.h>
 using namespace std;
-    int prior1=700;
-    int prior2=600;
-    int prior3=500;
-    int prior4=400;
-    int prior5=300;
-    int prior6=200;
-    int prior7=100;
+
+const int prior1 = 700;
+const int prior2 = 600;
+const int prior3 = 500;
+const int prior4 = 400;
+const int prior5 = 300;
+const int prior6 = 200;
+const int prior7 = 100;
 struct node
 {
     char sno[10];
@@ -24,22 +25,21 @@ struct node
 
 void display(node *n)
 {
-    cout<<n->sno<<" "<<n->time<<" "<<n->source<<" "<<n->destination<<" "<<n->protocol<<" "<<n->length<<" "<<n->info<<endl;
-    cout<<"PACKET PRIORITY : "<<n->key<<endl;
+    cout << n->sno << " " << n->time << " " << n->source << " " << n->destination << " " << n->protocol << " " << n->length << " " << n->info << endl;
+    cout << "PACKET PRIORITY : " << n->key << endl;
 }
-void save_copy(node *n1,node *n2)
+void save_copy(node *n1, node *n2)
 {
-    strcpy(n1->sno,n2->sno);
-    strcpy(n1->time,n2->time);
-    strcpy(n1->source,n2->source);
-    strcpy(n1->destination,n2->destination);
-    strcpy(n1->protocol,n2->protocol);
-    strcpy(n1->length,n2->length);
-    strcpy(n1->info,n2->info);
-    n1->key=n2->key;
-
+    strcpy(n1->sno, n2->sno);
+    strcpy(n1->time, n2->time);
+    strcpy(n1->source, n2->source);
+    strcpy(n1->destination, n2->destination);
+    strcpy(n1->protocol, n2->protocol);
+    strcpy(n1->length, n2->length);
+    strcpy(n1->info, n2->info);
+    n1->key = n2->key;
 }
-void exchange(node *n1,node *n2)
+void exchange(node *n1, node *n2)
 {
     char sno[10];
     char time[20];
@@ -48,120 +48,118 @@ void exchange(node *n1,node *n2)
     char protocol[10];
     char length[20];
     char info[400];
-    strcpy(sno,n1->sno);
-    strcpy(time,n1->time);
-    strcpy(source,n1->source);
-    strcpy(destination,n1->destination);
-    strcpy(protocol,n1->protocol);
-    strcpy(length,n1->length);
-    strcpy(info,n1->info);
+    strcpy(sno, n1->sno);
+    strcpy(time, n1->time);
+    strcpy(source, n1->source);
+    strcpy(destination, n1->destination);
+    strcpy(protocol, n1->protocol);
+    strcpy(length, n1->length);
+    strcpy(info, n1->info);
 
+    strcpy(n1->sno, n2->sno);
+    strcpy(n1->time, n2->time);
+    strcpy(n1->source, n2->source);
+    strcpy(n1->destination, n2->destination);
+    strcpy(n1->protocol, n2->protocol);
+    strcpy(n1->length, n2->length);
+    strcpy(n1->info, n2->info);
 
-    strcpy(n1->sno,n2->sno);
-    strcpy(n1->time,n2->time);
-    strcpy(n1->source,n2->source);
-    strcpy(n1->destination,n2->destination);
-    strcpy(n1->protocol,n2->protocol);
-    strcpy(n1->length,n2->length);
-    strcpy(n1->info,n2->info);
-
-    strcpy(n2->sno,sno);
-    strcpy(n2->time,time);
-    strcpy(n2->source,source);
-    strcpy(n2->destination,destination);
-    strcpy(n2->protocol,protocol);
-    strcpy(n2->length,length);
-    strcpy(n2->info,info);
-    int k=n1->key;
-    n1->key=n2->key;
-    n2->key=k;
-
+    strcpy(n2->sno, sno);
+    strcpy(n2->time, time);
+    strcpy(n2->source, source);
+    strcpy(n2->destination, destination);
+    strcpy(n2->protocol, protocol);
+    strcpy(n2->length, length);
+    strcpy(n2->info, info);
+    int k = n1->key;
+    n1->key = n2->key;
+    n2->key = k;
 }
 node *create_node(char *sent)
 {
-    node *n=new node;
+    node *n = new node;
 
-    char del[]=",";
-    char *no=strtok(sent,del);
-    char *ti=strtok(NULL,del);
-    char *sour=strtok(NULL,del);
-    char *dest=strtok(NULL,del);
-    char *prot=strtok(NULL,del);
-    char *len=strtok(NULL,del);
-    char *inf=strtok(NULL,del);
-    strcpy(n->sno,no);
-    strcpy(n->time,ti);
-    strcpy(n->source,sour);
-    strcpy(n->destination,dest);
-    strcpy(n->protocol,prot);
-    strcpy(n->length,len);
-    strcpy(n->info,inf);
-    if(strcmp(n->protocol,"\"TCP\"")==0)
+    char del[] = ",";
+    char *no = strtok(sent, del);
+    char *ti = strtok(NULL, del);
+    char *sour = strtok(NULL, del);
+    char *dest = strtok(NULL, del);
+    char *prot = strtok(NULL, del);
+    char *len = strtok(NULL, del);
+    char *inf = strtok(NULL, del);
+    strcpy(n->sno, no);
+    strcpy(n->time, ti);
+    strcpy(n->source, sour);
+    strcpy(n->destination, dest);
+    strcpy(n->protocol, prot);
+    strcpy(n->length, len);
+    strcpy(n->info, inf);
+    if (strcmp(n->protocol, "\"TCP\"") == 0)
     {
-   //     cout<<"\n------Packet Priority 1-------\n";
-        n->key=prior1--;
-        if(prior1==600)
+        //     cout<<"\n------Packet Priority 1-------\n";
+        n->key = prior1--;
+        if (prior1 == 600)
         {
-            prior1=700;
+            prior1 = 700;
         }
     }
-    else if(strcmp(n->protocol,"\"TLsv1\"")==0)
+    else if (strcmp(n->protocol, "\"TLsv1\"") == 0)
     {
-    //    cout<<"\n------Packet Priority 2-------\n";
-        n->key=prior2--;
-        if(prior2==500)
+        //    cout<<"\n------Packet Priority 2-------\n";
+        n->key = prior2--;
+        if (prior2 == 500)
         {
-            prior2=600;
+            prior2 = 600;
         }
     }
-    else if(strcmp(n->protocol,"\"SSDP\"")==0)
+    else if (strcmp(n->protocol, "\"SSDP\"") == 0)
     {
-   //     cout<<"\n------Packet Priority 3-------\n";
-        n->key=prior3--;
-        if(prior3==400)
+        //     cout<<"\n------Packet Priority 3-------\n";
+        n->key = prior3--;
+        if (prior3 == 400)
         {
-            prior3=500;
+            prior3 = 500;
         }
     }
-    else if(strcmp(n->protocol,"\"NBNS\"")==0)
+    else if (strcmp(n->protocol, "\"NBNS\"") == 0)
     {
-   //     cout<<"\n------Packet Priority 4-------\n";
-        n->key=prior4--;
-        if(prior4==300)
+        //     cout<<"\n------Packet Priority 4-------\n";
+        n->key = prior4--;
+        if (prior4 == 300)
         {
-            prior4=400;
+            prior4 = 400;
         }
     }
-    else if(strcmp(n->protocol,"\"QVIC\"")==0)
+    else if (strcmp(n->protocol, "\"QVIC\"") == 0)
     {
-    //    cout<<"\n------Packet Priority 5-------\n";
-        n->key=prior5--;
-        if(prior5==200)
+        //    cout<<"\n------Packet Priority 5-------\n";
+        n->key = prior5--;
+        if (prior5 == 200)
         {
-            prior5=300;
+            prior5 = 300;
         }
     }
-    else if(strcmp(n->protocol,"\"MDNS\"")==0)
+    else if (strcmp(n->protocol, "\"MDNS\"") == 0)
     {
-   //     cout<<"\n------Packet Priority 6-------\n";
-        n->key=prior6--;
-        if(prior6==100)
+        //     cout<<"\n------Packet Priority 6-------\n";
+        n->key = prior6--;
+        if (prior6 == 100)
         {
-            prior6=200;
+            prior6 = 200;
         }
     }
     else
     {
-   //     cout<<"\n------Packet Priority 7-------\n";
-        n->key=prior7--;
-        if(prior7==0)
+        //     cout<<"\n------Packet Priority 7-------\n";
+        n->key = prior7--;
+        if (prior7 == 0)
         {
-            prior7=100;
+            prior7 = 100;
         }
     }
 
-    n->left=NULL;
-    n->right=NULL;
+    n->left = NULL;
+    n->right = NULL;
 }
 class linked_list
 {
@@ -174,26 +172,26 @@ public:
     int get_count();
     void insert_end(char *);
     void delete_last();
-    int operator [](int);
+    int operator[](int);
     int get_key();
     void display();
-    node * get_node(int);
-    void set_key(int,int);
+    node *get_node(int);
+    void set_key(int, int);
 };
 int linked_list::get_key()
 {
     return end->key;
 }
-void linked_list::set_key(int key,int index)
+void linked_list::set_key(int key, int index)
 {
-    node *n=get_node(index);
-    n->key=key;
+    node *n = get_node(index);
+    n->key = key;
 }
 linked_list::linked_list()
 {
-    count=0;
-    start=NULL;
-    end=NULL;
+    count = 0;
+    start = NULL;
+    end = NULL;
 }
 int linked_list::get_count()
 {
@@ -202,49 +200,49 @@ int linked_list::get_count()
 void linked_list::insert_end(char *sent)
 {
     count++;
-    node *n=create_node(sent);
-    if(start==NULL)
+    node *n = create_node(sent);
+    if (start == NULL)
     {
-        start=n;
-        end=n;
+        start = n;
+        end = n;
         return;
     }
-    end->right=n;
-    n->left=end;
-    end=n;
+    end->right = n;
+    n->left = end;
+    end = n;
 }
 void linked_list::delete_last()
 {
-    if(count==1)
+    if (count == 1)
     {
-        delete(end);
-        start=end=NULL;
+        delete (end);
+        start = end = NULL;
         count--;
     }
-    else if(end!=NULL)
+    else if (end != NULL)
     {
-        node *n=end;
-        end=end->left;
-        end->right=NULL;
-        delete(n);
+        node *n = end;
+        end = end->left;
+        end->right = NULL;
+        delete (n);
         count--;
     }
 }
 int linked_list::operator[](int index)
 {
-    if(index<=1)
+    if (index <= 1)
     {
         return start->key;
     }
-    else if(index>=count)
+    else if (index >= count)
     {
         return end->key;
     }
-    node *n=start;
-    index=index-1;
-    while(index>0 && n!=NULL)
+    node *n = start;
+    index = index - 1;
+    while (index > 0 && n != NULL)
     {
-        n=n->right;
+        n = n->right;
         index--;
     }
     return n->key;
@@ -252,29 +250,30 @@ int linked_list::operator[](int index)
 
 void linked_list::display()
 {
-    node *n=start;
-    while(n!=NULL)
+    node *n = start;
+    while (n != NULL)
     {
-        cout<<"[" << n->protocol << "-" << n->sno <<"-"<<n->key<<"]" << "\n";
-        n=n->right;
+        cout << "[" << n->protocol << "-" << n->sno << "-" << n->key << "]"
+             << "\n";
+        n = n->right;
     }
-    cout<<"NULL"<<endl;
+    cout << "NULL" << endl;
 }
 node *linked_list::get_node(int index)
 {
-    if(index<=1)
+    if (index <= 1)
     {
         return start;
     }
-    else if(index>=count)
+    else if (index >= count)
     {
         return end;
     }
-    node *n=start;
+    node *n = start;
     index--;
-    while(index>0 && n!=NULL)
+    while (index > 0 && n != NULL)
     {
-        n=n->right;
+        n = n->right;
         index--;
     }
     return n;
--- main.cpp
+++ main.cpp
@@ -9,36 +9,38 @@ int tree_array_size = 20;
 int heap_size = 0;
 const int INF = 100000;
 linked_list A;
-int packets_handled=1;
+int packets_handled = 1;
+
 auto g_lock()
 {
     static mutex m;
     return unique_lock<decltype(m)>(m);
 }
 
-void string_to_char(string value,char *t)
+void string_to_char(string value, char *t)
 {
-    int n=value.length(),i;
- //   cout<<"n="<<n<<endl;
-    for(i=0;i<n;i++)
+    int n = value.length(), i;
+    //   cout<<"n="<<n<<endl;
+    for (i = 0; i < n; i++)
     {
-        t[i]=value[i];
-        if(value[i]==',')
+        t[i] = value[i];
+        if (value[i] == ',')
         {
-            if(value[i+1]=='"' && value[i-1]=='"')
+            if (value[i + 1] == '"' && value[i - 1] == '"')
             {
-                t[i]=value[i];
+                t[i] = value[i];
             }
             else
             {
-                t[i]='-';
+                t[i] = '-';
             }
         }
-     //   cout<<value[i];
+        //   cout<<value[i];
     }
-    t[i]='\0';
+    t[i] = '\0';
 }
-void swap( int *a, int *b ) {
+void swap(int *a, int *b)
+{
     int t;
     t = *a;
     *a = *b;
@@ -47,66 +49,64 @@ void swap( int *a, int *b ) {
 
 int get_right_child(linked_list &l, int index)
 {
-    if((((2*index)+1) <= l.get_count()) && (index >= 1))
+    if ((((2 * index) + 1) <= l.get_count()) && (index >= 1))
     {
-        return (2*index)+1;
+        return (2 * index) + 1;
     }
     return -1;
 }
-
 
 int get_left_child(linked_list &l, int index)
 {
-    if(((2*index) <= l.get_count()) && (index >= 1))
+    if (((2 * index) <= l.get_count()) && (index >= 1))
     {
-        return 2*index;
+        return 2 * index;
     }
     return -1;
 }
-
 
 int get_parent(linked_list &l, int index)
 {
     if ((index > 1) && (index <= l.get_count()))
     {
-        return index/2;
+        return index / 2;
     }
     return -1;
 }
 
 void max_heapify(linked_list &A, int index)
 {
-    int left_child_index = get_left_child(A,index);
-    int right_child_index = get_right_child(A,index);
-    node *left_=A.get_node(left_child_index);
-    node *right_=A.get_node(right_child_index);
+    int left_child_index = get_left_child(A, index);
+    int right_child_index = get_right_child(A, index);
+    node *left_ = A.get_node(left_child_index);
+    node *right_ = A.get_node(right_child_index);
     int largest = index;
-    node *largest_=A.get_node(largest);
-    if ((left_child_index <= heap_size) && (left_child_index>0))
+    node *largest_ = A.get_node(largest);
+    if ((left_child_index <= heap_size) && (left_child_index > 0))
     {
         if (A[left_child_index] > A[largest])
         {
             largest = left_child_index;
         }
-        else if(A[left_child_index]==A[largest])
+        else if (A[left_child_index] == A[largest])
         {
-            if(strcmp(left_->time,largest_->time)<0)
+            if (strcmp(left_->time, largest_->time) < 0)
             {
-                largest=left_child_index;
+                largest = left_child_index;
             }
         }
     }
-    if ((right_child_index <= heap_size && (right_child_index>0)))
+    if ((right_child_index <= heap_size && (right_child_index > 0)))
     {
         if (A[right_child_index] > A[largest])
         {
             largest = right_child_index;
         }
-        else if(A[right_child_index]==A[largest])
+        else if (A[right_child_index] == A[largest])
         {
-            if(strcmp(right_->time,largest_->time)<0)
+            if (strcmp(right_->time, largest_->time) < 0)
             {
-                largest=right_child_index;
+                largest = right_child_index;
             }
         }
     }
@@ -121,7 +121,7 @@ void max_heapify(linked_list &A, int index)
 void build_max_heap(linked_list &A)
 {
     int i;
-    for(i=heap_size/2; i>=1; i--)
+    for (i = heap_size / 2; i >= 1; i--)
     {
         max_heapify(A, i);
     }
@@ -132,22 +132,23 @@ int maximum(linked_list &A)
     return A[1];
 }
 
-node* extract_max(linked_list &A) {
-//    cout<<"\nNumber of Element  : "<<A.get_count()<<endl;
-    if(A.get_count()==1)
+node *extract_max(linked_list &A)
+{
+    //    cout<<"\nNumber of Element  : "<<A.get_count()<<endl;
+    if (A.get_count() == 1)
     {
-        node *save=new node;
-        save_copy(save,A.get_node(1));
+        node *save = new node;
+        save_copy(save, A.get_node(1));
         A.delete_last();
-      //  cout<<"\ndeleted...\n";
+        //  cout<<"\ndeleted...\n";
         return save;
     }
-    else if(A.get_count()>1)
+    else if (A.get_count() > 1)
     {
-        node *save=new node;
-        save_copy(save,A.get_node(1));
+        node *save = new node;
+        save_copy(save, A.get_node(1));
         //  A[1] = A[heap_size];
-        exchange(A.get_node(1),A.get_node(heap_size));
+        exchange(A.get_node(1), A.get_node(heap_size));
 
         heap_size--;
         A.delete_last();
@@ -158,90 +159,87 @@ node* extract_max(linked_list &A) {
 
 void increase_key(linked_list &A, int index, int key)
 {
- //   cout<<index<<endl;
-//    A.set_key(key,index);
-    while((index>1) && (A[get_parent(A, index)] < A[index]))
+    //   cout<<index<<endl;
+    //    A.set_key(key,index);
+    while ((index > 1) && (A[get_parent(A, index)] < A[index]))
     {
         exchange(A.get_node(index), A.get_node(get_parent(A, index)));
         index = get_parent(A, index);
-    //    A.display();
-       // cin.get();
-       // cin.get();
+        //    A.display();
+        // cin.get();
+        // cin.get();
     }
-  //  cout<<"hellooo";
+    //  cout<<"hellooo";
 }
 
-
-void insert(linked_list &A,char *sent) {
+void insert(linked_list &A, char *sent)
+{
     heap_size++;
     A.insert_end(sent);
-//    A.display();
-    //cout<<"hii";
+    //    A.display();
+    // cout<<"hii";
     increase_key(A, heap_size, A.get_key());
 }
 
 void print_heap(linked_list &A)
 {
     int i;
-    for(i=1; i<=heap_size; i++)
+    for (i = 1; i <= heap_size; i++)
     {
-        printf("%d\n",A[i]);
+        printf("%d\n", A[i]);
     }
     printf("\n");
 }
 void receive_packets()
 {
-    cout<<"\n---------------------";
-    cout<<"\n###Starting Router###\n";
-    cout<<"---------------------\n";
+    cout << "\n---------------------";
+    cout << "\n###Starting Router###\n";
+    cout << "---------------------\n";
     std::this_thread::sleep_for(200ms);
     string sent;
     sent.resize(1000);
     char sentence[1000];
     ifstream file("1.csv");
-    int count=0;
-    getline(file,sent);
-    while(getline(file,sent))
+    int count = 0;
+    getline(file, sent);
+    while (getline(file, sent))
     {
-        cout<<"Receiving Packet : ";
+        cout << "Receiving Packet : ";
         using namespace std::literals;
         std::this_thread::sleep_for(5ms);
-        string_to_char(sent,sentence);
-        insert(A,sentence);
-        cout<<"\n\n??????Packets Handled : "<<packets_handled++<<endl;
-
+        string_to_char(sent, sentence);
+        insert(A, sentence);
+        cout << "\n\n??????Packets Handled : " << packets_handled++ << endl;
     }
-
 }
 void send_packets()
 {
     std::this_thread::sleep_for(400ms);
 
-    while(A.get_count()!=0)
+    while (A.get_count() != 0)
     {
-        cout<<"\n#########################################CURRENT QUEUE ###"<<"##############################     "<<A.get_count()<<endl;
-        cout<<"Re-Transmitting Packet : \n";
+        cout << "\n#########################################CURRENT QUEUE ###"
+             << "##############################     " << A.get_count() << endl;
+        cout << "Re-Transmitting Packet : \n";
         display(extract_max(A));
         using namespace std::literals;
         std::this_thread::sleep_for(10ms);
-
     }
-
 }
 int main()
 {
-    time_t start,end;
-    start=clock();
+    time_t start, end;
+    start = clock();
     thread t1(receive_packets);
     thread t2(send_packets);
     t1.join();
     t2.join();
-    end=clock();
-    cout<<packets_handled<<endl;
-    time_t elapsed=(end-start)/1000.0;
-    cout<<"\n\nPACKETS HANDLED : "<<packets_handled<<endl;
-    cout<<"\nTime Elapsed : "<<(end-start)/1000.0<<"seconds;  AVERAGE PACKETS HANDLED PER SECOND : "<<(float)packets_handled/elapsed<<endl;
-  //  A.display();
+    end = clock();
+    cout << packets_handled << endl;
+    time_t elapsed = (end - start) / 1000.0;
+    cout << "\n\nPACKETS HANDLED : " << packets_handled << endl;
+    cout << "\nTime Elapsed : " << (end - start) / 1000.0 << "seconds;  AVERAGE PACKETS HANDLED PER SECOND : " << (float)packets_handled / elapsed << endl;
+    //  A.display();
 
-  return 0;
+    return 0;
 }
