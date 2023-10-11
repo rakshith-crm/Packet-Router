2c7cb42977d633eda5efef95d017814731e50f50
rakshithcrm <rakshithcrm@gmail.com>
2023-10-11 19:42:15 +0530

    changed

--- linked_list.h
+++ linked_list.h
@@ -189,10 +189,8 @@ void linked_list::set_key(int key, int index)
     node *n = get_node(index);
     n->key = key;
 }
-linked_list::linked_list()
+linked_list::linked_list() : start(nullptr), end(nullptr)
 {
-    start = nullptr;
-    end = nullptr;
 }
 int linked_list::get_count()
 {
